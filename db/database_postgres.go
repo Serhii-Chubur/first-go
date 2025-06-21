fc4f72c34a43f05335f1c5d900f7a808437bfd5e
Serhii-Chubur <serhichubur15@gmail.com>
2025-06-22 00:13:26 +0300

    implement CRUD

--- db/database_postgres.go
+++ db/database_postgres.go
@@ -29,6 +29,40 @@ func performQuery (conn *pgx.Conn, query string) {
 	}
 }
 
+func createUser(conn *pgx.Conn, name string, balance float32) {
+	_, err := conn.Exec(context.Background(), "INSERT INTO users (name, balance) VALUES ($1, $2)", name, balance)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("User %s created with balance %f\n\n", name, balance)
+}
+
+func getUserById (conn *pgx.Conn, id int) User{
+	var user User
+	err := conn.QueryRow(context.Background(), "SELECT * FROM users WHERE id = $1", id).Scan(&user.ID, &user.Name, &user.Balance)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("User ID: %d, Name: %s, Balance: %f\n", user.ID, user.Name, user.Balance)
+	return user
+}
+
+func updateBalance(conn *pgx.Conn, name string, balance float32) {
+	_, err := conn.Exec(context.Background(), "UPDATE users SET balance = $1 WHERE name = $2", balance, name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("User %s balance updated to %f\n\n", name, balance)
+}
+
+func deleteUser(conn *pgx.Conn, name string) {
+	_, err := conn.Exec(context.Background(), "DELETE FROM users WHERE name = $1", name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("User %s deleted\n\n", name)
+}
+
 func queryData(conn *pgx.Conn) {
 	rows, err := conn.Query(context.Background(), "SELECT * FROM users")
 	if err != nil {
@@ -55,6 +89,7 @@ func main() {
 	// performQuery(conn, "INSERT INTO users (name, balance) VALUES ('John Doe', 1000)")
 	// performQuery(conn, "INSERT INTO users (name, balance) VALUES ('Jane Doe', 2000)")
 	// performQuery(conn, "INSERT INTO users (name, balance) VALUES ('Andrew Black', 3000)")
+	createUser(conn, "Amy White", 7000)
 	defer conn.Close(context.Background())
 	queryData(conn)
 }
